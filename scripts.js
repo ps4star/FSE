7d41e7fbeca7bf915f363a17039f1661e3f75033
ps4star <hoenn4life@gmail.com>
2020-07-07 15:55:32 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,30 +1,40 @@
 var allScripts = {
 	
-	"obtain-poke" :
+	"stdlib": {
+	
+		"obtain_poke" :
 
 `fanfare 0x13E
-msgbox You received a %ARG_1! 0x4`,
+msgbox You received a %ARG1! 0x4`,
 
-	"name-poke" :
-	
-`msg Would you like to give a nickname to %ARG_1? 0x5
+		"name_poke" :
+		
+`msg Would you like to give a nickname to %ARG1? 0x5
 compare 0x800D 0x1
-if 0x1 call %ARG_2`,
+if 0x1 call %ARG2`,
 
-	"obtain-and-name" :
-	
-`fcall obtain-poke %ARG_1
-fcall name-poke %ARG_1 %ARG_2`,
+		"obtain_and_name" :
+		
+`fcall stdlib.obtain_poke %ARG1
+fcall stdlib.name_poke %ARG1 %ARG2`,
 
-	"nickname" :
-	
+		"nickname" :
+		
 `countpokemon
 subvar 0x800D 0x1
 copyvar 0x8004 0x800D
 fadescreen 0x1
 special 0x166
-waitstate`
+waitstate`,
 
+		"set_seen":
+		
+`setvar 0x8004 %ARG1P
+special 0x163`
 
+	}
 
 }
+
+
+
